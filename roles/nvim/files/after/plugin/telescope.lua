4eab2a18ef4747d9b664c26acd28d975095686b6
favilo <kevin.oberlies@elastic.co>
2024-04-09 13:06:08 -0700

    Migrate to alacritty.toml

--- roles/nvim/files/after/plugin/telescope.lua
+++ roles/nvim/files/after/plugin/telescope.lua
@@ -16,6 +16,7 @@ function vim.getVisualSelection()
 end
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('v', '<leader>pf', function() builtin.find_files({ default_text = vim.getVisualSelection() }) end, {})
 vim.keymap.set('n', '<leader>pp', function() builtin.find_files({ hidden = true }) end, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>po', builtin.oldfiles, {})
