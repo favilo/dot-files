759087f37a8d7311b2538ba28bcbd5fb8fa50cef
Kevin Oberlies <favilo@gmail.com>
2026-06-30 13:32:39 -0700

    feat: install rust-analyzer, lspmux with systemd service and add neovim copilot integration
    Signed-off-by: Kevin Oberlies <favilo@gmail.com>

--- roles/nvim/files/lua/plugins/spec/misc.lua
+++ roles/nvim/files/lua/plugins/spec/misc.lua
@@ -81,4 +81,14 @@ return {
       })
     end,
   },
+  {
+    "0xferrous/ansi.nvim",
+    config = function()
+      require("ansi").setup({
+        auto_enable = false, -- Auto-enable for configured filetypes
+        auto_enable_stdin = true, -- Auto-enable for piped stdin content
+        filetypes = { "log", "ansi" },
+      })
+    end,
+  },
 }
