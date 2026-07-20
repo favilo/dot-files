b686d53ab8b0a2ecb5474627efa8fb8186959aeb
Kevin Oberlies <favilo@gmail.com>
2026-07-20 15:22:08 -0700

    feat: add WezTerm and Neovim database UI
    Signed-off-by: Kevin Oberlies <favilo@gmail.com>

---
+++ roles/wezterm/files/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.keys = {
+  {
+    key = 'r',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ReloadConfiguration,
+  },
+}
+config.enable_tab_bar = false
+config.adjust_window_size_when_changing_font_size = false
+
+return config
