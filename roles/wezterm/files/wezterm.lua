6a3ce397385068892e2eef57c1e880f784c85b8f
Kevin Oberlies <favilo@gmail.com>
2026-07-29 12:03:50 -0700

    chore: add project conventions and tool configuration
    Signed-off-by: Kevin Oberlies <favilo@gmail.com>

--- roles/wezterm/files/wezterm.lua
+++ roles/wezterm/files/wezterm.lua
@@ -8,6 +8,7 @@ config.keys = {
     action = wezterm.action.ReloadConfiguration,
   },
 }
+config.enable_kitty_keyboard = true
 config.enable_tab_bar = false
 config.adjust_window_size_when_changing_font_size = false
 config.scrollback_lines = 50000
